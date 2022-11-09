58749695892fa0daec01589ffae6c4a0ae669210
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-11-09 19:56:36 +0000

    changed settings to allow login

--- backendbistro/menudb/models.py
+++ backendbistro/menudb/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class MenuItem(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    #category_id = models.ForeignKey('Category', on_delete=models.PROTECT)
-    #cuisine_id = models.ForeignKey('Cuisine', on_delete=models.PROTECT)
+    category_id = models.ForeignKey('Category', on_delete=models.PROTECT)
+    cuisine_id = models.ForeignKey('Cuisine', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
